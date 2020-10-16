ed95b7b21c0c34bd79501e27d740887d4133450e
Rafael Lima <rafaellimagms@gmail.com>
2020-10-16 14:20:06 -0300

    Apply CRUD operations

--- src/app/models/member.js
+++ src/app/models/member.js
@@ -53,10 +53,42 @@ module.exports = {
         })
 
     },
-    update() {
+    update(data, callback) {
+
+        const query = `
+            UPDATE members
+            SET avatar_url=($1),
+                name=($2),
+                birth=($3),
+                gender=($4),
+                blood=($5),
+                weight=($6),
+                height=($7),
+                email=($8)
+            WHERE id = $9
+            RETURNING id
+        `
+
+        db.query(query, data, function(err, results) {
+            if (err) throw `Database ${err}`
+
+            callback(results.rows[0])
+        })
 
     },
-    delete() {
+    delete(id, callback) {
+
+        const query = `
+            DELETE FROM members
+            WHERE id = $1
+        `
+
+        db.query(query, [id], function(err, results) {
+            if (err) throw `Database ${err}`
+
+            callback()
+        })
+
 
     }
 }
